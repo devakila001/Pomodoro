fa82b65044eeb45c345b3c4af8fbdaee33c37966
Akila Peramatchi <dev.akila001@outlook.com>
2024-06-21 01:10:39 +0530

    Created Models and DbContext

---
+++ Pomodoro.Server/Models/Entry.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pomodoro.Server.Models
+{
+    [Table("entry")]
+    public class Entry
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("entry_name")]
+        public string? Name { get; set; }
+
+        [Column("entry_date")]
+        public string? Date { get; set; }
+
+        [Column("start_time")]
+        public string? StartTime { get; set; }
+
+        [Column("end_time")]
+        public string? EndTime { get; set;}
+        [Column("comments")]
+        public string? Comments { get; set; }
+        [Column("total_time")]
+        public string? TotalTime { get; set; }
+                
+    }
+}
---
+++ Pomodoro.Server/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pomodoro.Server.Models
+{
+    [Table("user")]
+    public class User
+    {
+        [Key]
+        [Column("user_id")]
+        public int Id { get; set; }
+
+        [Column("user_name")]
+        public string? UserName { get; set; }
+
+        [Column("password")]
+        public string? Password { get; set; }
+
+    }
+}
